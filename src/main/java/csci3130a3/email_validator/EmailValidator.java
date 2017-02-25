df965c00ff599ec4529caffd33347cfb2332a8f7
Aimee <Aimee@DESKTOP-06A6TF6>
2017-02-25 15:12:16 -0400

    1

---
+++ src/main/java/csci3130a3/email_validator/EmailValidator.java
@@ -0,0 +1,45 @@
+package csci3130a3.email_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmailValidator 
+{
+   public EmailValidator(){
+	   
+   }
+
+public boolean justOneAt(String ex1) {
+	
+	int i=0;
+	for(int j=0;j<ex1.length();j++){
+		if(ex1.charAt(j)=='@'){
+			i++;
+		}
+	}
+	if(i != 1){
+		return false;
+	}
+	return true;
+}
+
+public boolean moreonePoint(String ex1) {
+	int i=0;
+	for(int j=0;j<ex1.length();j++){
+		if(ex1.charAt(j)=='.'){
+			i++;
+		}
+	}
+	if(i >= 1){
+		return true;
+	}
+	return false;
+}
+
+public boolean isValidate(String ex1) {
+	boolean result=false;
+	result=this.justOneAt(ex1)&&this.moreonePoint(ex1);
+	return result;
+}
+}
---
+++ src/test/java/csci3130a3/email_validator/EmailValidatorTest.java
@@ -0,0 +1,78 @@
+package csci3130a3.email_validator;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class EmailValidatorTest extends TestCase {
+	
+	@Test
+	public void testConstructor(){
+		EmailValidator validator=new EmailValidator();
+		assertNotNull(validator);
+	}
+	
+	@Test
+	public void testAt1(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=true;
+		String ex1="try@test.com";
+		boolean r=validator.justOneAt(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void testAtNull(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=false;
+		String ex1="trytest.com";
+		boolean r=validator.justOneAt(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void testAtMore(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=false;
+		String ex1="try@@test.com";
+		boolean r=validator.justOneAt(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void testPoint1(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=true;
+		String ex1="try@test.com";
+		boolean r=validator.moreonePoint(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void testPointNull(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=false;
+		String ex1="try@testcom";
+		boolean r=validator.moreonePoint(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void testPointMore(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=true;
+		String ex1="try@test..com";
+		boolean r=validator.moreonePoint(ex1);
+		assertFalse(r != report);
+	}
+	
+	@Test
+	public void isValidate(){
+		EmailValidator validator=new EmailValidator();
+		boolean report=true;
+		String ex1="try@test.com";
+		boolean r=validator.isValidate(ex1);
+		assertFalse(r != report);
+	}
+
+}
